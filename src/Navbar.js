fa51332cbf1261056695a30baf7ff36410576f19
asafstr2 <asafstr2@gmail.com>
2020-03-19 00:30:19 +0200

    fixed home

--- src/Navbar.js
+++ src/Navbar.js
@@ -24,9 +24,9 @@ export default class Navbar extends Component {
 					<div className="collapse navbar-collapse" id="navbarSupportedContent">
 						<ul className="navbar-nav mr-auto">
 							<li className="nav-item active">
-								<a className="nav-link" href="#">
+								<NavLink className="nav-link" to="/">
 									Home <span className="sr-only">(current)</span>
-								</a>
+								</NavLink>
 							</li>
 							<li className="nav-item">
 								<a className="nav-link" href="#">
