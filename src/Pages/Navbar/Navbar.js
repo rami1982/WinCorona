b42155e82133c4e5df46cdc31bb190a86a3d9186
james <jjbe.93@gmail.com>
2020-03-30 08:50:42 +0300

    converting to react hooks

--- src/Pages/Navbar/Navbar.js
+++ src/Pages/Navbar/Navbar.js
@@ -1,105 +1,104 @@
-import React, { Component } from 'react';
+import React from "react";
 import { NavLink } from "react-router-dom";
 
-import { connect } from 'react-redux';
-import * as actionType from '../../Store/actions';
+import { connect } from "react-redux";
+import * as actionType from "../../Store/actions";
 
-export class Navbar extends Component {
-	render() {
-		return (
-			<div>
-				<nav className="  navbar navbar-expand-lg navbar-light bg-light ">
-					<NavLink className="navbar-brand" to="/">
-						Win Corona
-					</NavLink>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-toggle="collapse"
-						data-target="#navbarSupportedContent"
-						aria-controls="navbarSupportedContent"
-						aria-expanded="false"
-						aria-label="Toggle navigation"
-					>
-						<span className="navbar-toggler-icon" />
-					</button>
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="  navbar navbar-expand-lg navbar-light bg-light ">
+        <NavLink className="navbar-brand" to="/">
+          Win Corona
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
 
-					<div className="collapse navbar-collapse" id="navbarSupportedContent">
-						<ul className="navbar-nav mr-auto">
-							<li className="nav-item active">
-								<NavLink className="nav-link" to="/">
-									Home <span className="sr-only">(current)</span>
-								</NavLink>
-							</li>
-							<li className="nav-item active">
-								{/* <NavLink className="nav-link " to="/formpage">
-									Form 
-								</NavLink> */}
-							</li>
-							<li className="nav-item active">
-								{/* <NavLink className="nav-link " to="/Onetimeform">
-									One Time Form 
-								</NavLink> */}
-							</li>
-							{/*<li className="nav-item">
-								<a className="nav-link" href="#">
-									Link
-								</a>
-							</li>
-							<li className="nav-item dropdown">
-								<a
-									className="nav-link dropdown-toggle"
-									href="#"
-									id="navbarDropdown"
-									role="button"
-									data-toggle="dropdown"
-									aria-haspopup="true"
-									aria-expanded="false"
-								>
-									Dropdown
-								</a>
-								<div className="dropdown-menu" aria-labelledby="navbarDropdown">
-									<a className="dropdown-item" href="#">
-										Action
-									</a>
-									<a className="dropdown-item" href="#">
-										Another action
-									</a>
-									<div className="dropdown-divider" />
-									<a className="dropdown-item" href="#">
-										Something else here
-									</a>
-								</div>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link disabled" href="#">
-									Disabled
-								</a>
-							</li> */}
-						</ul>
-						{/* <span class="navbar-text">
-						<NavLink className="nav-link " to="/signin">
-     						 Sign-in/Login 
-							  </NavLink>
-    					</span> */}
-					</div>
-				</nav>
-			</div>
-		);
-	}
-}
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <NavLink className="nav-link" to="/">
+                Home <span className="sr-only">(current)</span>
+              </NavLink>
+            </li>
+            <li className="nav-item active">
+              <NavLink className="nav-link " to="/formpage">
+                Form
+              </NavLink>
+            </li>
+            <li className="nav-item active">
+              <NavLink className="nav-link " to="/Onetimeform">
+                One Time Form
+              </NavLink>
+            </li>
+            {/* <li className="nav-item">
+              <a className="nav-link" href="#">
+                Link
+              </a>
+            </li>
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-toggle="dropdown"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Dropdown
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                <a className="dropdown-item" href="#">
+                  Action
+                </a>
+                <a className="dropdown-item" href="#">
+                  Another action
+                </a>
+                <div className="dropdown-divider" />
+                <a className="dropdown-item" href="#">
+                  Something else here
+                </a>
+              </div>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#">
+                Disabled
+              </a>
+            </li> */}
+          </ul>
+          {/* <span class="navbar-text">
+            <NavLink className="nav-link " to="/signin">
+              Sign-in/Login
+            </NavLink>
+          </span> */}
+        </div>
+      </nav>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
-	return {
-		page: state.page,
-		pageTitle: state.pageTitle,
-	}
-}
+  return {
+    page: state.page,
+    pageTitle: state.pageTitle
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-	return {
-		switchPageStatus: (page) => dispatch({type: actionType.CURRENT_PAGE, pageName: page}),
-	}
-}
+  return {
+    switchPageStatus: page =>
+      dispatch({ type: actionType.CURRENT_PAGE, pageName: page })
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(Navbar);
--- src/Pages/OneTimeForm/oneTimeForm.js
+++ src/Pages/OneTimeForm/oneTimeForm.js
@@ -1,186 +1,197 @@
-import React, { Component } from 'react';
-import './oneTimeForm.css';
+import React from "react";
+import "./oneTimeForm.css";
 
-import { connect } from 'react-redux';
-import * as actionType from '../../Store/actions';
+import { connect } from "react-redux";
+import * as actionType from "../../Store/actions";
 
-export class OneTimeForm extends Component {
+const OneTimeForm = props => {
+  // state = {
+  // }
 
-    state = {
-        
-    }
+  // BUTTONS HANDLERS
 
+  const ageHandler = () => {
+    console.log("ageHandler");
+  };
 
+  const genderHandler = () => {
+    console.log("genderHandler");
+  };
 
+  const cityHandler = () => {
+    console.log("cityHandler");
+  };
 
-    // BUTTONS HANDLERS
+  const diseasesHandler = () => {
+    console.log("diseasesHandler");
+  };
 
-    ageHandler = () => {
-        console.log("ageHandler");
-    }
+  const smokeingHandler = () => {
+    console.log("smokeingHandler");
+  };
 
-    genderHandler = () => {
-        console.log("genderHandler")
-    }
+  const isolationHandler = () => {
+    console.log("capsOfTeaHandler");
+  };
 
+  const feelingHandler = () => {
+    console.log("soscialHandler");
+  };
 
+  const peopleHandler = () => {
+    console.log("peopleHandler");
+  };
 
-    cityHandler = () => {
-        console.log("cityHandler");
-    }
+  // BUTTONS HANDLERS
 
-    diseasesHandler = () => {
-        console.log("diseasesHandler");
-    }
+  return (
+    <div className="formWrapper">
+      {/* age */}
+      <div className="basic age">
+        <label>מה הגיל שלך</label>
+        <input type="text" onChange={ageHandler} />
+      </div>
 
-    smokeingHandler = () => {
-        console.log("smokeingHandler");
-    }
+      <div className="basic gender">
+        <label>?בן? בת</label>
+        <div>
+          <label>בן &nbsp;&nbsp;</label>
+          <input
+            type="radio"
+            name="a"
+            onChange={genderHandler}
+            defaultChecked
+          />
+        </div>
+        <div>
+          <label>בת &nbsp;&nbsp;</label>
+          <input type="radio" name="a" onChange={genderHandler} />
+        </div>
+      </div>
 
-    isolationHandler = () => {
-        console.log("capsOfTeaHandler");
-    }
+      <div className="basic city">
+        <label>מה הוא מקום המגורים שלך</label>
+        <input type="text" onChange={cityHandler} />
+      </div>
 
-    feelingHandler = () => {
-        console.log("soscialHandler");
-    }
+      <div className="basic diseases">
+        <label>
+          האם היה לך את אחת המחלות הבאות: סוכרת, בעיית יתר לחץ דם, מחלות לב או
+          כלי דם או שבץ, מחלות ריאות כרוניות, סרטן, אי ספיקת כליות
+        </label>
+        <div>
+          <label>כן &nbsp;&nbsp;</label>
+          <input
+            type="radio"
+            name="a"
+            onChange={diseasesHandler}
+            defaultChecked
+          />
+        </div>
+        <div>
+          <label>לא &nbsp;&nbsp;</label>
+          <input type="radio" name="a" onChange={diseasesHandler} />
+        </div>
+      </div>
 
-    peopleHandler = () => {
-        console.log("peopleHandler");
-    }
+      <div className="basic smokeing">
+        <label>מה לגבי עישון</label>
+        <div>
+          <label>עישון יומיומי &nbsp;&nbsp;</label>
+          <input
+            type="radio"
+            name="a"
+            onChange={smokeingHandler}
+            defaultChecked
+          />
+        </div>
+        <div>
+          <label>עישנתי בעבר, בחמש השנים האחרונות &nbsp;&nbsp;</label>
+          <input type="radio" name="a" onChange={smokeingHandler} />
+        </div>
+        <div>
+          <label>עישנתי בעבר, לפני יותר מחמש שנים &nbsp;&nbsp;</label>
+          <input type="radio" name="a" onChange={smokeingHandler} />
+        </div>
+        <div>
+          <label>אף פעם &nbsp;&nbsp;</label>
+          <input type="radio" name="a" onChange={smokeingHandler} />
+        </div>
+      </div>
 
+      <div className="basic isolation">
+        <label>האם היית בבידוד</label>
+        <div>
+          <label>אני לא בבידוד &nbsp;&nbsp;</label>
+          <input
+            type="radio"
+            name="a"
+            onChange={isolationHandler}
+            defaultChecked
+          />
+        </div>
+        <div>
+          <label>אני בבידוד כי חזרתי מחו"ל לאחרונה &nbsp;&nbsp;</label>
+          <input type="radio" name="a" onChange={isolationHandler} />
+        </div>
+        <div>
+          <label>אני בבידוד כי נחשפתי לחולה מאומת &nbsp;&nbsp;</label>
+          <input type="radio" name="a" onChange={isolationHandler} />
+        </div>
+        <div>
+          <label>אני נמצא בבידוד כי חוויתי תסמינים &nbsp;&nbsp;</label>
+          <input type="radio" name="a" onChange={isolationHandler} />
+        </div>
+        <div>
+          <label>אני נמצא בבידוד מרצוני החופשי &nbsp;&nbsp;</label>
+          <input type="radio" name="a" onChange={isolationHandler} />
+        </div>
+        <div>
+          <label>אני מאושפז &nbsp;&nbsp;</label>
+          <input type="radio" name="a" onChange={isolationHandler} />
+        </div>
+      </div>
 
+      <div className="basic feeling">
+        <label>מה שלומך היום</label>
+        <div>
+          <label>מצויין &nbsp;&nbsp;</label>
+          <input
+            type="radio"
+            name="a"
+            onChange={feelingHandler}
+            defaultChecked
+          />
+        </div>
+        <div>
+          <label>לא כל כך טוב &nbsp;&nbsp;</label>
+          <input type="radio" name="a" onChange={feelingHandler} />
+        </div>
+      </div>
 
-    
-
-    // BUTTONS HANDLERS
-
-
-	render() {
-		return (
-			<div className="formWrapper">
-
-           
-                
-
-                    {/* age */}
-                    <div className="basic age">
-                        <label>מה הגיל שלך</label>
-                        <input type="text" onChange={this.ageHandler}/>
-                    </div>
-
-                    <div className="basic gender">
-                        <label>?בן? בת</label>
-                        <div>
-                            <label>בן &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.genderHandler} defaultChecked/>
-                        </div>
-                        <div>
-                            <label>בת &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.genderHandler}/>
-                        </div>
-                    </div>
-
-                    <div className="basic city">
-                        <label>מה הוא מקום המגורים שלך</label>
-                        <input type="text" onChange={this.cityHandler}/>
-                    </div>
-
-                    <div className="basic diseases">
-                        <label>האם היה לך את אחת המחלות הבאות: סוכרת, בעיית יתר לחץ דם, מחלות לב או כלי דם או שבץ, מחלות ריאות כרוניות, סרטן, אי ספיקת כליות</label>
-                        <div>
-                            <label>כן &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.diseasesHandler} defaultChecked/>
-                        </div>
-                        <div>
-                            <label>לא &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.diseasesHandler}/>
-                        </div>
-                    </div>
-
-                    <div className="basic smokeing">
-                        <label>מה לגבי עישון</label>
-                        <div>
-                            <label>עישון יומיומי &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.smokeingHandler} defaultChecked/>
-                        </div>
-                        <div>
-                            <label>עישנתי בעבר, בחמש השנים האחרונות &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.smokeingHandler}/>
-                        </div>
-                        <div>
-                            <label>עישנתי בעבר, לפני יותר מחמש שנים &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.smokeingHandler}/>
-                        </div>
-                        <div>
-                            <label>אף פעם &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.smokeingHandler}/>
-                        </div>
-                    </div>
-
-                    <div className="basic isolation">
-                        <label>האם היית בבידוד</label>
-                        <div>
-                            <label>אני לא בבידוד &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.isolationHandler} defaultChecked/>
-                        </div>
-                        <div>
-                            <label>אני בבידוד כי חזרתי מחו"ל לאחרונה &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.isolationHandler}/>
-                        </div>
-                        <div>
-                            <label>אני בבידוד כי נחשפתי לחולה מאומת &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.isolationHandler}/>
-                        </div>
-                        <div>
-                            <label>אני נמצא בבידוד כי חוויתי תסמינים &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.isolationHandler}/>
-                        </div>
-                        <div>
-                            <label>אני נמצא בבידוד מרצוני החופשי &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.isolationHandler}/>
-                        </div>
-                        <div>
-                            <label>אני מאושפז &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.isolationHandler}/>
-                        </div>
-                    </div>
-
-                    <div className="basic feeling">
-                        <label>מה שלומך היום</label>
-                        <div>
-                            <label>מצויין &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.feelingHandler} defaultChecked/>
-                        </div>
-                        <div>
-                            <label>לא כל כך טוב &nbsp;&nbsp;</label>
-                            <input type='radio' name='a' onChange={this.feelingHandler}/>
-                        </div>
-                    </div>
-
-                    <div className="basic people">
-                        <label>מה מספר האנשים השונים (לא כולל בני ביתך) ששהית בקרבתם מעל ל-5 דקות ביממה האחרונה</label>
-                        <input type="text" onChange={this.peopleHandler}/>
-                    </div>
-
-
-                </div>
-
-		);
-	}
-}
+      <div className="basic people">
+        <label>
+          מה מספר האנשים השונים (לא כולל בני ביתך) ששהית בקרבתם מעל ל-5 דקות
+          ביממה האחרונה
+        </label>
+        <input type="text" onChange={peopleHandler} />
+      </div>
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
-	return {
-		page: state.page,
-		pageTitle: state.pageTitle,
-	}
-}
+  return {
+    page: state.page,
+    pageTitle: state.pageTitle
+  };
+};
 
 const mapDispatchToProps = dispatch => {
-	return {
-		switchPageStatus: (page) => dispatch({type: actionType.CURRENT_PAGE, pageName: page}),
-	}
-}
+  return {
+    switchPageStatus: page =>
+      dispatch({ type: actionType.CURRENT_PAGE, pageName: page })
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(OneTimeForm);
