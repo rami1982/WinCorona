5a0d4a3091f4e7730fc8914bf4ca70ae86909541
yoniivan <yonievan@gmail.com>
2020-04-26 10:12:39 +0300

    fix generic chatbox

--- src/Pages/FormPage/FormDaily.js
+++ src/Pages/FormPage/FormDaily.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 import "./formDaily.css";
 import { ThemeProvider } from 'styled-components';
-
-import {questions} from './questions'
+import ChantbotJSON from './questions';
 import { connect } from "react-redux";
 import * as actionType from "../../Store/actions";
 import ChatBot from 'react-simple-chatbot';
+import api from '../../Store/api';
 
 const theme = {
   background: '#f5f8fb',
@@ -19,39 +19,175 @@ const theme = {
   userFontColor: '#4a4a4a',
 };
 
+// chatBot initialize.
+let chatbot = new ChantbotJSON();
+
+// question array * THIS IS WHERE YOU ADD/REMOVE QUESTIONS !!!
+const jsonQuestions = [
+  // 0
+  {
+    message: 'האם יש לך תסמינים של שפעת/וירוס/קורונה ',
+    opt: [
+      { value: 'yes', label: 'כן'},
+      { value: 'no', label: 'לא'},
+    ],
+  },
+  // 1
+  {
+    message:'?האם יש לך קורונה',
+    opt: [
+      { value: 'yes', label: 'כן'},
+      { value: 'no', label: 'לא'},
+    ],
+  },
+  // 2
+  {
+    message: 'האם אתה לוקח תרופות באופן קבוע',
+    opt: [
+      { value: 'yes', label: 'כן'},
+      { value: 'no', label: 'לא'},
+    ],
+  },
+  // 3
+  {
+    message: 'מה החום גוף הכי גבוהה שהיה לך בשבוע האחרון',
+    opt: [
+      { value: 'dont know', label: 'לא מדדתי' },
+      { value: '37.9', label: '37.9' },
+      { value: '38.5', label: '38.5' },
+      { value: 'over 38.5', label: 'מעל 38.5' },
+    ],
+  },
+  // 4
+  {
+    message: 'איך המצב הבריאותי שלך בשבוע האחרון?',
+    opt: [
+      { value: 'better', label: 'השתפר' },
+      { value: 'worse', label: 'התדרדר' },
+      { value: 'stable', label: 'נשאר יציב' },
+    ],
+  },
+  // 5
+  {
+    message:'?איזה אוכל אתה אוכל',
+    opt: [
+      { value: 'all', label: 'הכל' },
+      { value: 'vegan', label: 'טבעוני' },
+      { value: 'veggie', label: 'צמחוני' },
+      { value: 'non-gloten', label: 'ללא גלוטן' },
+    ],
+  },
+  // 6
+  {
+    message:'?איזה פעילויות עשית בשבוע האחרון ',
+    opt: [
+      { value: 'sport', label: 'ספורט' },
+      { value: 'yoga', label: 'יוגה' },
+      { value: 'meditation', label: 'מדיטציה' },
+      { value: 'prayer', label: 'תפילה' },
+    ],
+  },
+  // 7
+  {
+    message:'?כמה שעות שינה בממוצע את/ה ישן ביום',
+    opt: [
+      { value: 'lessThan6', label: 'פחות מ- 6 שעות' },
+      { value: '6to7', label: '6-7' },
+      { value: '7-8', label: '7-8' },
+      { value: 'over8', label: 'מעל 8' },
+    ],
+  },
+  // 8
+  {
+    message:'?מה מצב הרוח שלך',
+    opt: [
+      { value: 'verygood', label: 'טוב מאוד' },
+      { value: 'good', label: 'טוב' },
+      { value: 'medium', label: 'בינוני' },
+      { value: 'low', label: 'נמוך' },
+      { value: 'verylow', label: 'נמוך מאוד' },
+    ],
+  },
+  // 9
+  {
+    message:'?מה מצב האנרגיה והמוטיבציה שלך',
+    opt: [
+      { value: 'high', label: 'בעל מוטיבציה גבוהה' },
+      { value: 'hodaya', label: 'בהודיה על החיים' },
+      { value: 'tired', label: 'עייפות' },
+      { value: 'anxiety', label: 'חרדה' },
+      { value: 'nervous', label: 'עצבנות' },
+      { value: 'sensitiv', label: 'רגישות' },
+      { value: 'indifference', label: 'אדישות' },
+    ],
+  },
+  // 10
+  {
+    message:'האם יש משהו מהתסמינים האלה?',
+    opt: [
+      { value: 'cough', label: 'שיעולים או נזלת' },
+      { value: 'throat', label: 'כאבי גרון' },
+      { value: 'breath', label: 'קוצר נשימה' },
+      { value: 'pain', label: 'כאבים' },
+      { value: 'diarrhea', label: 'שלשול' },
+      { value: 'nausea', label: 'בחילה או הקאות' },
+      { value: 'chills', label: 'צמרצורת' },
+      { value: 'confusion', label: 'בלבול' },
+      { value: 'tiredness', label: 'עייפות' },
+      { value: 'infrmity', label: 'עייפות או חולשה' },
+      { value: 'taste', label: 'איבדתי את חוש הטעם והריח' },
+      { value: 'none', label: 'אף אחד מאלו' },
+    ],
+  },
+  // 11
+  {
+    message:'גובה בס"מ',
+  },
+  // 12
+  {
+    message:'משקל בק"ג',
+  },
+]
+
+// big-array of all question that is given to the ChatBot Component.
+let arrQuestions = []
+
+const count = jsonQuestions.length - 1;
+
+// add's all questions from 'jsonQuestions' into 'arrQuestions'.
+jsonQuestions.map((arr, index) => {
+  if(arr.hasOwnProperty('opt')){
+    arrQuestions.push(...chatbot.mulAns(arr));
+  }else if(jsonQuestions.length - 1 === index){
+    arrQuestions.push(...chatbot.singleAns(arr, true));
+  }else{
+    arrQuestions.push(...chatbot.singleAns(arr));
+  }
+})
+
 const FormPage = props => {
 
-  const inputState = useState({
-    id_1: '',
-    id_2: '',
-    id_3: '',
-    id_4: '',
-    id_5: '',
-    id_6: '',
-    id_7: '',
-    id_8: '',
-    id_9: '',
-  });
-
+  
   //handle end callback from chatbots
   function handleEnd({ steps, values }) {
+    const jsonObject = new Object();
+    values.map((v, index) => {
+      const key = jsonQuestions[index].message
+      jsonObject[key] = v;
+    });
+    // AXIOS
+    api.post('http://localhost:3001/answers/daily', jsonObject).then(data => {
+      console.log(data);
+    }).catch(err => {
+      console.log(err);
+    })
 
-    inputState.id_1 = values[0];
-    inputState.id_2 = values[1];
-    inputState.id_3 = values[2];
-    inputState.id_4 = values[3];
-    inputState.id_5 = values[4];
-    inputState.id_6 = values[5];
-    inputState.id_7 = values[6];
-    inputState.id_8 = values[7];
-    inputState.id_9 = values[8];
-
-    alert(`Chat handleEnd callback! Number: ${values[0]}`);
+    console.log(values);
+    alert(`תודה רבה על מילוי השאלון`);
   }
 
 
   return (
-
     <div style={{width:'100%',margin:'10px auto',maxWidth:'500px'}}>
       {/* Radio button */}
       <ThemeProvider theme={theme}>
@@ -59,7 +195,7 @@ const FormPage = props => {
         width="100%"
         hideHeader
         botAvatar="/images/docFace.png"
-        steps={questions}
+        steps={arrQuestions}
         handleEnd={handleEnd}
         />
           </ThemeProvider>
--- src/Pages/FormPage/questions.js
+++ src/Pages/FormPage/questions.js
@@ -1,117 +1,48 @@
-export const questions = [
-  {
-    id: '1',
-    message:'האם יש לך תסמינים של שפעת/וירוס/קורונה ',
-    trigger: '2'
-  },
-  {
-    id: '2',
-    options: [
-        { value: 'yes', label: 'כן', trigger: '3' },
-        { value: 'no', label: 'לא', trigger: '3' },
-      ],
-  },
-  {
-    id: '3',
-    message:'?האם יש לך קורונה',
-    trigger: '4'
-  },
-{
-id: '4',
-options: [
-    { value: 'yes', label: 'כן', trigger: '5' },
-    { value: 'no', label: 'לא', trigger: '5' },
-  ],
-},
-{
-    id: '5',
-    message:'?מה החום גוף הכי גבוהה שהיה לך בשבוע האחרון',
-    trigger: '6'
-  },
-{
-id: '6',
-options: [
-    { value: 'dont know', label: 'לא מדדתי', trigger: '7' },
-    { value: '37.9', label: '37.9', trigger: '7' },
-    { value: '38.5', label: '38.5', trigger: '7' },
-    { value: 'over 38.5', label: 'מעל 38.5', trigger: '7' },
-  ],
-},
-{
-    id: '7',
-    message:'?איזה אוכל אתה אוכל',
-    trigger: '8'
-  },
-{
-id: '8',
-options: [
-    { value: 'all', label: 'הכל', trigger: '9' },
-    { value: 'vegan', label: 'טבעוני', trigger: '9' },
-    { value: 'veggie', label: 'צמחוני', trigger: '9' },
-    { value: 'non-gloten', label: 'ללא גלוטן', trigger: '9' },
-  ],
-},
-{
-    id: '9',
-    message:'?איזה פעילויות עשית בשבוע האחרון ',
-    trigger: '10'
-  },
-{
-id: '10',
-options: [
-    { value: 'sport', label: 'ספורט', trigger: '11' },
-    { value: 'yoga', label: 'יוגה', trigger: '11' },
-    { value: 'meditation', label: 'מדיטציה', trigger: '11' },
-    { value: 'prayer', label: 'תפילה', trigger: '11' },
-  ],
-},
-{
-    id: '11',
-    message:'?כמה שעות שינה בממוצע את/ה ישן ביום',
-    trigger: '12'
-  },
-{
-id: '12',
-options: [
-    { value: 'lessThan6', label: 'פחות מ- 6 שעות', trigger: '13' },
-    { value: '6to7', label: '6-7', trigger: '13' },
-    { value: '7-8', label: '7-8', trigger: '13' },
-    { value: 'over8', label: 'מעל 8', trigger: '13' },
-  ],
-},
-{
-    id: '13',
-    message:'?מה מצב הרוח שלך',
-    trigger: '14'
-},
-{
-id: '14',
-options: [
-    { value: 'verygood', label: 'טוב מאוד', trigger: '15' },
-    { value: 'good', label: 'טוב', trigger: '15' },
-    { value: 'medium', label: 'בינוני', trigger: '15' },
-    { value: 'low', label: 'נמוך', trigger: '15' },
-    { value: 'verylow', label: 'נמוך מאוד', trigger: '15' },
-  ],
-},
-{
-    id: '15',
-    message:'גובה בס"מ',
-    trigger: '16'
-},
-{
-    id: '16',
-    user: true,
-    trigger: '17',
-  },
-  {
-    id: '17',
-    message:'משקל בק"ג',
-    trigger: '18'
-},
-  {
-    id: '18',
-    user: true,
-    end: true,
-  },
-]
+export default class ChantbotJSON {
+  constructor(){
+      this.n = 1;
+  }
+
+  mulAns(q){
+      const question = {
+          id: `${this.n}`,
+          message: `${q.message}`,
+          trigger: `${this.n + 1}`
+      }
+      
+      const answer = {}
+      q.opt.map(tri => {
+              tri["trigger"] = `${this.n + 2}`;
+          })
+      answer["id"] = `${this.n + 1}`;
+      answer["options"] = q.opt;
+
+      this.n = this.n + 2
+  
+      return [question, answer]
+  }
+
+  singleAns(q, isLast = false){
+      const question = {
+          id: `${this.n}`,
+          message:`${q.message}`,
+          trigger: `${this.n + 1}`,
+      }
+      
+      const answer = {}
+
+      if (isLast){
+          answer["id"] = `${this.n + 1}`;
+          answer["user"] = true;
+          answer["end"] = true;
+      }else{
+          answer["id"] = `${this.n + 1}`;
+          answer["user"] = true;
+          answer["trigger"] = `${this.n + 2}`;
+      }
+
+      this.n = this.n + 2
+
+      return [question, answer]
+  }
+}
